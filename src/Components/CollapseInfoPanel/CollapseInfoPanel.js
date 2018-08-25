6d14453fcefb6c528ce35cdb716ef1574f8491eb
estivensal7 <estivensal7@gmail.com>
2018-08-25 18:57:17 -0400

    added media queries for the collapseInfoPanel file which is used in collpasing more information in the interior and exterior pages

--- src/Components/CollapseInfoPanel/CollapseInfoPanel.js
+++ src/Components/CollapseInfoPanel/CollapseInfoPanel.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Jumbotron } from 'reactstrap';
+import './CollapseInfoPanel.css';
 
 const CollapseInfoPanel = (props) => {
   return (
-    <div>
-      <Jumbotron>
+    <div className="collapse-info-panel-container">
+      <Jumbotron className="collapse-info-panel-jumbotron">
         <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
       </Jumbotron>
     </div>
