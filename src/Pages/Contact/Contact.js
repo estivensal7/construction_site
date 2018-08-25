932d69aa0a3c89d4edfc592630cbae5d67d8e403
estivensal7 <estivensal7@gmail.com>
2018-08-25 15:12:34 -0400

    finished basic implementation of contact page with text area that utilizes the 'mailto:' method as well as a jumbotron to hold further contact information.

--- src/Pages/Contact/Contact.js
+++ src/Pages/Contact/Contact.js
@@ -3,9 +3,11 @@ import {
 	Button, 
 	Form, 
 	FormGroup, 
-	Label, 
-	Input, 
-	FormText } from 'reactstrap';
+	Input,
+	Row,
+	Col,
+	Jumbotron,
+	Container } from 'reactstrap';
 import './Contact.css';
 
 export default class Contact extends Component {
@@ -23,25 +25,42 @@ export default class Contact extends Component {
     render() {
     	return (
 			<div className="contactContainer">
-				<Form>
-					<FormGroup>
-					<Input 
-						type="textarea" 
-						name="message" 
-						id="exampleText" 
-						placeholder="Send us a Message!"
-						onChange={this.handleChange}
-					/>
-					</FormGroup>
-					<Button>
-						<a 
-							href ={`mailto:estivensal7@gmail.com?subject=New Message&body=${this.state.message}`}
-							style={{textDecoration: 'none', color: 'white'}}
-						>
-							Send Message
-						</a>
-					</Button>
-				</Form>
+				<Row>
+					<Col>
+						{/* Jumbotron will be used to hold contact information, and better instructions on how to contact company */}
+						<Jumbotron fluid>
+							<Container fluid>
+							<h1 className="display-3">Fluid jumbotron</h1>
+							<p className="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
+							</Container>
+						</Jumbotron>
+					</Col>
+				</Row>
+				<Row>
+					<Col>	
+						{/* In this form we can utilize the text area to write a message which when sent will utilize the 'mailto:' method to open the user's
+						mailbox and draft a message that can be sent to the company's email */}
+						<Form>
+							<FormGroup>
+							<Input 
+								type="textarea" 
+								name="message" 
+								id="exampleText" 
+								placeholder="Send us a Message!"
+								onChange={this.handleChange}
+							/>
+							</FormGroup>
+							<Button>
+								<a 
+									href ={`mailto:estivensal7@gmail.com?subject=New Message&body=${this.state.message}`}
+									style={{textDecoration: 'none', color: 'white'}}
+								>
+									Send Message
+								</a>
+							</Button>
+						</Form>
+					</Col>
+				</Row>
 			</div>
       	)
     }
